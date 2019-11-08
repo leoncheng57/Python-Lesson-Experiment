af48ba66661aea8803f6854e6557773f0f98cfce
Leon Cheng <57leonardo@gmail.com>
2019-11-08 14:13:14 -0800

    more flask setup

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Index Page
 @app.route('/')
 def index():
-    return "this is the index page"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
