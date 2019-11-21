761ac217e0063668c54cc14a373c2589c9434604
Leon Cheng <57leonardo@gmail.com>
2019-11-20 20:38:53 -0800

    copy over lessons

---
+++ Lessons/Unanthropomorphized/Lesson1a.py
@@ -0,0 +1,30 @@
+"""
+
+**********
+LESSON 1A
+**********
+
+In the following code (lines 12-17), change x to 10, and then print out whether
+or not x is 10.
+
+"""
+
+x = 5
+
+if x == 5:
+    print("x is five!")
+else:
+    print("x is not five...")
+
+"""
+
+Expected solution:
+
+x = 10
+
+if x == 10:
+    print("x is ten!")
+else:
+    print("x is not ten...")
+
+"""
