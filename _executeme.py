8b5154cfdf8b63a39e570467ce64bc4f7709b74d
Leon Cheng <57leonardo@gmail.com>
2019-11-21 22:20:25 -0800

    removed answers

--- _executeme.py
+++ _executeme.py
@@ -1,9 +1,14 @@
 
 """
 
-**********
-LESSON 1B
-**********
+************
+LESSON 1B :)
+************
+
+Congratulations on making it this far!
+
+This time, we'll be letting go of the training wheels and asking you to write
+the code fully on your own (at line 24)!
 
 In the following space, use the if/elif/else structure to:
 	1) Set a variable, 'day', to the current day of the week as a string.
@@ -11,34 +16,12 @@ In the following space, use the if/elif/else structure to:
 		Tuesday, and so on ... for all the weekdays (not including weekends)
 	3) If it a weekend (Saturday, Sunday), print "It's the weekend!"
 
-"""
-
-print("Hello world")
-
-        <div class="stdoutContainer">
-            <h3>Output</h3>
-            <p id="stdoutDisplay">(...)</p>
-        </div>
-
+Good luck!
+Rachel
 
 """
 
-Expected solution:
+# Write your code here!
+print("Hello world!")
 
-day = "Sunday" # This can be any day of the week
-
-if day == "Monday":
-	print("It is Monday!")
-elif day == "Tuesday":
-	print("It is Tuesday!")
-elif day == "Wednesday":
-	print("It is Wednesday!")
-elif day == "Thursday":
-	print("It is Thursday!")
-elif day == "Friday":
-	print("It is Friday!")
-else:
-	print("It's the weekend!")
-
-"""
 
